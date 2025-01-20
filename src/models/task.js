cfcb4fe1be2b78013cb829f54de74f592a915ebc
Cris <c.cheuquel01@ufromail.cl>
2025-01-20 10:10:06 -0300

    Task model done

--- src/models/task.js
+++ src/models/task.js
@@ -5,7 +5,11 @@ const mongoose = require('mongoose');
 // ----------------------------------------------------
 
 cont taskSchema = new mongoose.Schema({
+  user_id: {type: String, required: true},
   name: {type: String, required: true},
   desciption: {type: String, required: false},
   status: {type: String, required: true}
 });
+
+const task = mongoose.model('Task', taskSchema);
+module.exports = task;
